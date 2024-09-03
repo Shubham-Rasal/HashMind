d402d1e01b180183cb9d5e03b9212e39d64f1013
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-09-03 23:52:50 +0530

    scaffold next app and add lit action

---
+++ backend/index.js
@@ -0,0 +1,286 @@
+import express from "express"
+
+import { encryptString, decryptToString, LitNodeClient } from "@lit-protocol/lit-node-client";
+import { Wallet } from "ethers";
+import {
+    LitActionResource,
+    LitAccessControlConditionResource,
+    LitAbility,
+    createSiweMessageWithRecaps,
+    generateAuthSig,
+} from "@lit-protocol/auth-helpers"
+import { LitNetwork } from "@lit-protocol/constants";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 8000;
+
+const client = new LitNodeClient({
+    litNetwork: LitNetwork.DatilDev,
+    debug: true,
+});
+
+const walletWithCapacityCredit = new Wallet(
+    process.env.PRIVATE_KEY
+);
+
+const authSig = await (async () => {
+    const toSign = await createSiweMessageWithRecaps({
+        uri: "http://localhost",
+        expiration: new Date(Date.now() + 1000 * 60 * 60).toISOString(), // 24 hours
+        walletAddress: walletWithCapacityCredit.address,
+        nonce: await client.getLatestBlockhash(),
+        resources: [
+            {
+                resource: new LitActionResource('*'),
+                ability: LitAbility.LitActionExecution,
+            },
+            {
+                resource: new LitAccessControlConditionResource('*'),
+                ability: LitAbility.AccessControlConditionDecryption,
+            }
+        ],
+        litNodeClient: client,
+    });
+    return await generateAuthSig({
+        signer: walletWithCapacityCredit,
+        toSign,
+    });
+})();
+
+export class Lit {
+    litNodeClient;
+    accessControlConditions;
+    chain;
+
+    constructor(client, chain, accessControlConditions) {
+        this.litNodeClient = client;
+        this.chain = chain;
+        this.accessControlConditions = accessControlConditions;
+    }
+
+    async connect() {
+        // disconnectWeb3();
+        await this.litNodeClient.connect()
+    }
+
+    async disconnect() {
+        await this.litNodeClient.disconnect()
+    }
+
+    async getSessionSigsServer() {
+        const latestBlockhash = await this.litNodeClient.getLatestBlockhash();
+
+        const authNeededCallback = async (params) => {
+            if (!params.uri) {
+                throw new Error("uri is required");
+            }
+            if (!params.expiration) {
+                throw new Error("expiration is required");
+            }
+
+            if (!params.resourceAbilityRequests) {
+                throw new Error("resourceAbilityRequests is required");
+            }
+
+            const toSign = await createSiweMessageWithRecaps({
+                uri: params.uri,
+                expiration: params.expiration,
+                resources: params.resourceAbilityRequests,
+                walletAddress: walletWithCapacityCredit.address,
+                nonce: latestBlockhash,
+                litNodeClient: this.litNodeClient,
+            });
+
+            const authSig = await generateAuthSig({
+                signer: walletWithCapacityCredit,
+                toSign,
+            });
+            // console.log("authSig:", authSig);
+            return authSig;
+        }
+
+        // const litResource = new LitAccessControlConditionResource('*');
+
+        const sessionSigs = await this.litNodeClient.getSessionSigs({
+            chain: this.chain,
+            resourceAbilityRequests: [
+                {
+                    resource: new LitActionResource('*'),
+                    ability: LitAbility.LitActionExecution,
+                },
+                {
+                    resource: new LitAccessControlConditionResource('*'),
+                    ability: LitAbility.AccessControlConditionDecryption,
+                }
+            ],
+            authNeededCallback,
+        });
+        console.log("sessionSigs:", sessionSigs);
+        return sessionSigs
+    }
+
+    async encrypt(message) {
+        console.log("encrypting message: ", message, typeof message);
+        const sessionSigs = await this.getSessionSigsServer();
+        console.log(sessionSigs)
+        const { ciphertext, dataToEncryptHash } = await encryptString(
+            {
+                accessControlConditions: this.accessControlConditions,
+                chain: this.chain,
+                dataToEncrypt: message,
+                sessionSigs,
+            },
+            this.litNodeClient,
+        );
+        return {
+            ciphertext,
+            dataToEncryptHash,
+        };
+    }
+
+    async decrypt(ciphertext, dataToEncryptHash) {
+        const sessionSigs = await this.getSessionSigsServer();
+        console.log(sessionSigs)
+
+        const decryptedString = await decryptToString(
+            {
+                accessControlConditions: [this.accessControlConditions],
+                chain: this.chain,
+                ciphertext,
+                dataToEncryptHash,
+                sessionSigs,
+                authSig,
+            },
+            this.litNodeClient,
+        );
+        return { decryptedString }
+    }
+
+    async decryptLitAction(ciphertext, dataToEncryptHash, mode) {
+        const code = `(async () => {
+            console.log("hello");
+            const privateKey = await Lit.Actions.decryptAndCombine({
+              accessControlConditions,
+              chain: "ethereum",
+              ciphertext,
+              dataToEncryptHash,
+              authSig,
+              sessionSigs
+            });
+            console.log("privateKey: ", privateKey);
+            Lit.Actions.setResponse({ response: privateKey });
+          })();`
+
+        const sessionSigs = await this.getSessionSigsServer();
+        // // Decrypt the private key inside a lit action
+        const res = await this.litNodeClient.executeJs({
+            sessionSigs,
+            code: code,
+            // code: genActionSource2(),
+            jsParams: {
+                accessControlConditions: this.accessControlConditions,
+                ciphertext,
+                dataToEncryptHash,
+                sessionSigs,
+                authSig
+            }
+        });
+        console.log("result from action execution:", res);
+
+        return res.response;
+    }
+}
+
+export const encryptRunServerMode = async (message) => {
+    const chain = "ethereum";
+
+    const accessControlConditions = [
+        {
+            conditionType: "evmBasic",
+            contractAddress: "",
+            standardContractType: "",
+            chain: 'polygon',
+            method: "eth_getBalance",
+            parameters: [":userAddress", "latest"],
+            returnValueTest: {
+                comparator: ">=",
+                value: "0",
+            },
+        },
+    ];
+
+    let myLit = new Lit(client, chain, accessControlConditions);
+    await myLit.connect();
+
+    const { ciphertext, dataToEncryptHash } = await myLit.encrypt(message, "server");
+    console.log("ciphertext: ", ciphertext);
+    console.log("dataToEncryptHash: ", dataToEncryptHash);
+
+    return { ciphertext, dataToEncryptHash };
+};
+
+export const decryptRunServerMode = async (dataToEncryptHash, ciphertext) => {
+    const chain = "ethereum";
+
+    const accessControlConditions = [
+        {
+            conditionType: "evmBasic",
+            contractAddress: "",
+            standardContractType: "",
+            chain: 'polygon',
+            method: "eth_getBalance",
+            parameters: [":userAddress", "latest"],
+            returnValueTest: {
+                comparator: ">=",
+                value: "0",
+            },
+        },
+    ];
+
+    let myLit = new Lit(client, chain, accessControlConditions);
+    // await myLit.connect();
+
+    const data = await myLit.decryptLitAction(ciphertext, dataToEncryptHash, "server");
+    console.log("decrypted data: ", data);
+
+    return (data)
+}
+
+export async function run() {
+    const message = "Hello, Lit Protocol!";
+
+    // Encrypt the message
+    const { ciphertext, dataToEncryptHash } = await encryptRunServerMode(message);
+
+    // Decrypt the message
+    const decryptedData = await decryptRunServerMode(dataToEncryptHash, ciphertext);
+    console.log("Decrypted Data:", decryptedData);
+    return decryptedData;
+}
+
+// run().catch(console.error);
+
+app.post('/encrypt', async (req, res) => {
+    const bodyData = req.body;
+    const { ciphertext, dataToEncryptHash } = await encryptRunServerMode(bodyData.message);
+
+    res.send({ "ciphertext": ciphertext, "dataToEncryptHash": dataToEncryptHash });
+});
+
+app.post('/decrypt', async (req, res) => {
+    const bodyData = req.body;
+    const decryptedData = await decryptRunServerMode(bodyData.dataToEncryptHash, bodyData.ciphertext);
+
+    res.send({ "decryptedData": decryptedData });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
