0f54a2134b835e9cbde3db0b9daa73b9e76973a5
marcdhi <gandhi.mardav@gmail.com>
2024-09-03 20:38:04 +0530

    message-kit: resolve gitguardian & env

--- message-kit/src/lib/redis.ts
+++ message-kit/src/lib/redis.ts
@@ -7,11 +7,6 @@ export const getRedisClient = async (): Promise<RedisClientType> => {
     url: process.env.REDIS_CONNECTION_STRING,
   });
 
-  // const client = new Redis({
-  //   url: 'https://probable-viper-51887.upstash.io',
-  //   token: 'AcqvAAIjcDEyNTY0NDY0MjhjNTQ0Y2ExYjMzY2ExOWFmNTc5ZDE2N3AxMA',
-  // })
-
 
   await client.connect();
   return client as RedisClientType;
