9872fc55cf03604851d15429270293b5748609b5
marcdhi <gandhi.mardav@gmail.com>
2024-09-04 01:26:39 +0530

    message-kit: make changes to questions

--- message-kit/dist/lib/redis.js
+++ message-kit/dist/lib/redis.js
@@ -3,10 +3,6 @@ export const getRedisClient = async () => {
     const client = createClient({
         url: process.env.REDIS_CONNECTION_STRING,
     });
-    // const client = new Redis({
-    //   url: 'https://probable-viper-51887.upstash.io',
-    //   token: 'AcqvAAIjcDEyNTY0NDY0MjhjNTQ0Y2ExYjMzY2ExOWFmNTc5ZDE2N3AxMA',
-    // })
     await client.connect();
     return client;
 };
